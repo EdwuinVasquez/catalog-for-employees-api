413a03b3eede62d816aebc7ad6530042ce3121e2
EdwuinVasquez <136001640+EdwuinVasquez@users.noreply.github.com>
2023-06-14 10:07:42 -0500

    Crear rutas de modificacion de usuario
    se crearon las siguientes rutas:
    -PUT /mod/usuario/estado: cambiar si el usuario es betado o no del sistema.
    -PUT /mod/usuario/verificado: modificar la verificacion del usuario.
    -PUT /mod/usuario/datos: modificar datos personales del usuario
    -PUT /mod/usuario/clave: cambiar la calve de ingreso.
    -PUT /mod/usuario/login: actualizar el ultimo inicio de sesion

--- flight-master/Put.php
+++ flight-master/Put.php
@@ -1,7 +1,51 @@
 <?php 
-	// Obtener informacion de usuarios 
-  Flight::route('GET /usuarios', function () {
-    $sentencia = Flight::db() ->prepare("CALL PROCEDURE_LISTAR_USUARIO('')");
+	// Actualizar estado del usuario
+	Flight::route('PUT /mod/usuario/estado', function () {
+		$cedula = Flight::request()->data->cedula;
+		$estado = Flight::request()->data->estado;
+    $sentencia = Flight::db() ->prepare("CALL PROCEDURE_MODIFICAR_ESTADOUSUARIO_USUARIO('$cedula','$estado')");
+		$sentencia->execute();
+		$datos=$sentencia->fetchAll();
+		Flight::json($datos);
+  });
+
+	// Actualizar estado de verificacion del usuario
+	Flight::route('PUT /mod/usuario/verificado', function () {
+		$cedula = Flight::request()->data->cedula;
+    $sentencia = Flight::db() ->prepare("CALL PROCEDURE_MODIFICAR_ESTADOVERIFICADO_USUARIO('$cedula')");
+		$sentencia->execute();
+		$datos=$sentencia->fetchAll();
+		Flight::json($datos);
+  });
+
+	// Actualizar informacion del usuario
+	Flight::route('PUT /mod/usuario/datos', function () {
+		$cedula = Flight::request()->data->cedula;
+		$nombre = Flight::request()->data->nombre;
+		$clave = Flight::request()->data->clave;
+		$pregunta = Flight::request()->data->pregunta;
+		$respuesta = Flight::request()->data->respuesta;
+		$contacto = Flight::request()->data->contacto;
+    $sentencia = Flight::db() ->prepare("CALL PROCEDURE_MODIFICAR_USUARIO('$cedula','$nombre','$clave','$pregunta','$respuesta','$contacto')");
+		$sentencia->execute();
+		$datos=$sentencia->fetchAll();
+		Flight::json($datos);
+  });
+
+	// Actualizar contraseña del usuario
+	Flight::route('PUT /mod/usuario/clave', function () {
+		$cedula = Flight::request()->data->cedula;
+		$clave = Flight::request()->data->clave;
+    $sentencia = Flight::db() ->prepare("CALL PROCEDURE_MODIFICAR_CLAVE_USUARIO('$cedula','$clave')");
+		$sentencia->execute();
+		$datos=$sentencia->fetchAll();
+		Flight::json($datos);
+  });
+
+	// Actualizar ultimo ingreso al sistema
+	Flight::route('PUT /mod/usuario/login', function () {
+		$cedula = Flight::request()->data->cedula;
+    $sentencia = Flight::db() ->prepare("CALL PROCEDURE_MODIFICAR_ULTIMOINGRESO_USUARIO('$cedula')");
 		$sentencia->execute();
 		$datos=$sentencia->fetchAll();
 		Flight::json($datos);
